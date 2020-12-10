5d65e4e3ec458324552d9539b9090b3e4bf4343a
David <eldeividlh@gmail.com>
2020-12-10 12:32:45 +0100

    Centramos el index.html para que este centrada toda la página + finalizamos template inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-import { CarouselComponent } from './components/home/carousel/carousel.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { FraseCelebreComponent } from './components/frase-celebre/frase-celebre.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { TimeTaskComponent } from './components/time-task/time-task.component';
@@ -17,13 +15,11 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     MenuComponent,
-    FraseCelebreComponent,
     LoginComponent,
     RegisterComponent,
     TimeTaskComponent,
     ListTaskComponent,
-    HomeComponent,
-    CarouselComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
