7d9d4e9143d0445b40cbac7e9692d514666d0669
David <eldeividlh@gmail.com>
2020-12-11 16:14:53 +0100

    Modifico validación de login está al 95%

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,27 +7,60 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  submitted = false;
+  sumitted = false;
 
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]   ),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    email: new FormControl('',
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+      ]
+    ),
+    password: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    )
   });
 
-  submitForm(){
-    this.loggerFormInfo();
+  submitForm(): void{
+    this.loggerFormAndFieldsInfo();
+    this.sumitted = true;
   }
 
-  loggerFormInfo(){
-    console.log("---------------------------------------------------");
-    this.loginForm.get('email')?.status === 'VALID' ? console.log("Email OK") : console.error("Email KO");
-    this.loginForm.get('password')?.status === 'VALID' ? console.log("Password OK") : console.error("Password KO");
+  loggerFormAndFieldsInfo(): void{
+    console.log('---------------------------------------------------');
+    this.loginForm.get('email')?.status === 'VALID' ? console.log('Email OK') : console.error('Email KO');
+    this.loginForm.get('password')?.status === 'VALID' ? console.log('Password OK') : console.error('Password KO');
     /* Form Validation */
-    this.loginForm?.status === 'VALID' ? console.log("Form OK") : console.error("Form KO");
-    console.log("---------------------------------------------------");
+    this.loginForm?.status === 'VALID' ? console.log('Form OK') : console.error('Form KO');
+    console.log('---------------------------------------------------');
   }
 
-  constructor(formBuilder : FormBuilder) { }
+  formInputColor(field: string): string{
+    if (this.sumitted === true){
+      if (this.loginForm?.get(field)?.status === 'VALID'){
+        return 'is-valid';
+      }
+    } else if (this.sumitted === false){
+      return ' ';
+    }
+    return 'is-invalid';
+  }
+
+  msgColor(field: string): boolean{
+    if (this.sumitted === true){
+      if (this.loginForm?.get(field)?.status === 'VALID'){
+        return false;
+      }
+    } else if (this.sumitted === false){
+      return false;
+    }
+    return true;
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
 
