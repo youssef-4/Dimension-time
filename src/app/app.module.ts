453964eea1308327968393f4086e623f13e56fb1
David <eldeividlh@gmail.com>
2020-12-13 10:23:22 +0100

    Añado las flechas de alante atrás en los formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment.prod';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
