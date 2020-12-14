a7edae12b488986a229231ca11a9d5409f8894a7
David <eldeividlh@gmail.com>
2020-12-14 19:47:22 +0100

    añado temporizador

--- src/app/components/time-task/time-task.component.ts
+++ src/app/components/time-task/time-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-time-task',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-task.component.css']
 })
 export class TimeTaskComponent implements OnInit {
+  stopCounter = false;
+  finishExecution = false;
+  nowDate: any;
+  startDate: any;
+  breaker = false;
+  buttonTxt = 'Start';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changesColorBtn(): boolean {
+    if (this.breaker === false){
+      this.buttonTxt = 'Stop';
+      return false;
+    } else{
+      this.buttonTxt = 'Start';
+      return true;
+    }
+    console.log('activamos botón');
   }
+
+  startTimmer(event: any): void{
+
+    this.breaker = !this.breaker;
+    this.startDate = new Date();
+    this.startDate = moment(this.startDate).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    // this.nowDate = moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a').fromNow();
+    setInterval(() => {
+      this.nowDate = moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a').fromNow();
+      moment.utc(moment(this.nowDate, 'dddd, MMMM Do YYYY, h:mm:ss a')
+      .diff(moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a'))).format('dd:hh:mm:ss');
+
+    }, 1000);
+
+    console.log(this.nowDate);
+
+
+  }
+
+  stopTimmer(): void{
+    this.stopCounter = true;
+  }
+
+  ngOnInit(): void {}
 
 }
