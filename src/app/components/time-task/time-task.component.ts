3a42623a4f00148d229dffc998f1b90b55b1cb38
David <eldeividlh@gmail.com>
2020-12-15 13:17:37 +0100

    Adding task in timeTask and show buttons

--- src/app/components/time-task/time-task.component.ts
+++ src/app/components/time-task/time-task.component.ts
@@ -8,26 +8,51 @@ import * as moment from 'moment';
 })
 export class TimeTaskComponent implements OnInit {
   nowDate = ' ';
-  startDate: any;
-  myInterval: any;
+  startDate: any = '';
+  myInterval: any = '';
   nameTask = '';
+  myLastTask = {
+    nameTask: '',
+    startTask : '',
+    timeTask : ''
+  };
+
+  showTask = false;
+  showBtnStart = true;
+  showBtnStop = false;
+
   constructor() {}
 
   stopTimmer(): void {
+      // Stop interval
       clearInterval(this.myInterval);
-      this.startDate = ' ';
-      this.nowDate = ' ';
+      // Set new values for the task
+      this.myLastTask.nameTask = this.nameTask;
+      this.myLastTask.startTask = this.startDate;
+      this.myLastTask.timeTask = this.nowDate;
+      // Reset task
+      this.startDate = '';
+      this.nowDate = '';
       this.nameTask = '';
+
+      this.showBtnStart = !this.showBtnStart;
+      this.showBtnStop = !this.showBtnStop;
+      this.showTask = true;
+      // true = false
   }
 
-  startTimmer(event: any): void{
-    this.startDate = new Date();
-    this.startDate = moment(this.startDate).format('dddd, MMMM Do YYYY, h:mm:ss a');
+  startTimmer(): void{
+    // Visible Last Task
+    this.showBtnStart = !this.showBtnStart;
+    this.showBtnStop = !this.showBtnStop;
+    // Start Date
+    this.startDate = moment(new Date()).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    // Now VS Start Date
     this.myInterval = setInterval(() => {
       this.nowDate = moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a').fromNow();
       moment.utc(moment(this.nowDate, 'dddd, MMMM Do YYYY, h:mm:ss a')
       .diff(moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a'))).format('dd:hh:mm:ss');
-    }, 1000);
+    }, 100);
   }
 
   ngOnInit(): void {}
