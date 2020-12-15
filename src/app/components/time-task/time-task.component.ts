98d20c7d9fd18ef7a13cdb20768fc044f368d9a5
David <eldeividlh@gmail.com>
2020-12-15 12:31:09 +0100

    Botones done

--- src/app/components/time-task/time-task.component.ts
+++ src/app/components/time-task/time-task.component.ts
@@ -7,44 +7,27 @@ import * as moment from 'moment';
   styleUrls: ['./time-task.component.css']
 })
 export class TimeTaskComponent implements OnInit {
-  stopCounter = false;
-  finishExecution = false;
-  nowDate: any;
+  nowDate = ' ';
   startDate: any;
-  breaker = false;
-  buttonTxt = 'Start';
+  myInterval: any;
+  nameTask = '';
   constructor() {}
 
-  changesColorBtn(): boolean {
-    if (this.breaker === true){
-      this.buttonTxt = 'Stop';
-      return false;
-    } else{
-      this.buttonTxt = 'Start';
-      this.startDate = null;
-      this.nowDate = null;
-      return true;
-    }
+  stopTimmer(): void {
+      clearInterval(this.myInterval);
+      this.startDate = ' ';
+      this.nowDate = ' ';
+      this.nameTask = '';
   }
 
   startTimmer(event: any): void{
-
-    this.breaker = !this.breaker;
     this.startDate = new Date();
     this.startDate = moment(this.startDate).format('dddd, MMMM Do YYYY, h:mm:ss a');
-    // this.nowDate = moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a').fromNow();
-    setInterval(() => {
+    this.myInterval = setInterval(() => {
       this.nowDate = moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a').fromNow();
       moment.utc(moment(this.nowDate, 'dddd, MMMM Do YYYY, h:mm:ss a')
       .diff(moment(this.startDate, 'dddd, MMMM Do YYYY, h:mm:ss a'))).format('dd:hh:mm:ss');
-
     }, 1000);
-
-    console.log(this.nowDate);
-  }
-
-  stopTimmer(): void{
-    this.stopCounter = true;
   }
 
   ngOnInit(): void {}
